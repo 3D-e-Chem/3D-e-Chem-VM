331374f1fedbcdc64232a64b25a1257139cf8f5f
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2016-02-24 17:16:32 +0100

    Use script to detect repo metadata.

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 
+
 # Created with
 
   docker run --rm --volume=$(pwd):/srv/jekyll -i -t  -p 127.0.0.1:4000:4000 jekyll/jekyll:pages jekyll new --force .
 
+# Generate config
+
+```
+pip install -r utils/requirements.txt
+python utils/gen_config.py <organization> <repo>
+```
+
+This will use the Github API to fetch information about the repo and write the `_config.yml` file. The `_config.yml` must be edited to complete the configuration.
+
 # Local hosting
 
-  docker run --rm --volume=$(pwd):/srv/jekyll -i -t  -p 127.0.0.1:4000:4000 jekyll/jekyll:pages
+```
+docker run --rm --volume=$(pwd):/srv/jekyll -i -t  -p 127.0.0.1:4000:4000 jekyll/jekyll:pages
+```
 
-This Jekyll website uses `site.github` var, see https://help.github.com/articles/repository-metadata-on-github-pages/,
-this var is not available in the docker container only on https://*.github.io
+The website will be hosted at `http://localhost:4000/<repo name>/`.
---
+++ utils/gen_config.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import argparse
+
+import github3
+import yaml
+
+parser = argparse.ArgumentParser(description='Generate Github variable for jekyll')
+parser.add_argument('organization', help='Github organization or user name')
+parser.add_argument('repository', help='Github repository name')
+parser.add_argument('configfn',
+                    default='_config.yml',
+                    type=argparse.FileType('w'),
+                    help="Filename for config file. Default _config.yml. Use - for stdout")
+args = parser.parse_args()
+
+gh = github3.GitHub()
+
+repo = gh.repository(args.organization, args.repository)
+
+config = {
+    'title': repo.name,
+    'description': repo.description,
+    'baseurl': '/' + repo.name,
+    'project_name': repo.owner.login,
+    'urls': {
+        'changelog': repo.html_url + '/blob/master/CHANGELOG.md',
+        'download': repo.html_url + '/releases/latest',
+        'issues': repo.html_url + '/issues',
+        'repo': repo.html_url,
+        'project': '<FIXME>',
+        'docs': repo.html_url + '/wiki',
+    },
+    'languages': [l[0] for l in repo.languages()],
+    'license': repo.license().license['name'],
+    'partners': [{
+        'logo_url': 'https://www.esciencecenter.nl/img/pressroom/ESCIENCE_logo_C_nl_cyanblack.jpg',
+        'url': 'https://www.esciencecenter.nl'
+    }, {
+        'logo_url': '<FIXME AND OPTIONALLY REPEATME>',
+        'url': '<FIXME AND OPTIONALLY REPEATME>'
+    }],
+    'markdown': 'kramdown',
+    'excludes': [
+        'README.md',
+        'utils',
+    ],
+    'keep_files': [
+        'images'
+    ]
+}
+
+yaml.dump(config, args.configfn, default_flow_style=False)
